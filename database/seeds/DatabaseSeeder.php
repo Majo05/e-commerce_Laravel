d153d389edfba499a13ccff0f0749c4fa53b6e4b
JENNIFER RUSH <jennifer.rush28@gmail.com>
2019-08-22 21:10:22 -0300

    campos de roles y doctypes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('doctypes')->insert([
+            'id' => 1,
+            'name' => 'DNI'
+        ]);
+        DB::table('doctypes')->insert([
+            'id' => 2,
+            'name' => 'Pasaporte'
+        ]);
+        DB::table('doctypes')->insert([
+            'id' => 3,
+            'name' => 'Libreta Civica'
+        ]);
+
+
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'Administrador'
+        ]);
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'Usuario'
+        ]);
     }
 }
